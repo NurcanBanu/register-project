781cb61e682db29350f68cc5fc81f5812a0e8249
nurcanbanugumus@gmail.com <160108787+NurcanBanu@users.noreply.github.com>
2025-04-30 21:58:00 +0300

    Router hatası düzeltildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 
-import { Route, Router, Routes } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './components/Login'
 import Success from './components/Success'
 function App() {
 
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/success" element={<Success />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
 
